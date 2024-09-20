0400c68af234afe4510a5aa6a9affb0cd6f56ed1
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-09-20 13:46:00 +0300

    setup

---
+++ src/main/java/mate/academy/skillshare/SkillShareApplication.java
@@ -0,0 +1,11 @@
+package mate.academy.skillshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillShareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkillShareApplication.class, args);
+    }
+}
---
+++ src/test/java/mate/academy/skillshare/SkillShareApplicationTests.java
@@ -0,0 +1,11 @@
+package mate.academy.skillshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillShareApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
