79d6e90def022461cc820c91fe6aefc9fe73bd9c
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-10-04 19:59:32 +0300

    CI fix 11.0

--- src/test/java/mate/academy/skillshare/SkillShareApplicationTests.java
+++ src/test/java/mate/academy/skillshare/SkillShareApplicationTests.java
@@ -1,12 +1,9 @@
 package mate.academy.skillshare;
 
-import mate.academy.skillshare.config.GlobalSetupExtension;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ExtendWith(GlobalSetupExtension.class)
 class SkillShareApplicationTests {
     @Test
     void contextLoads() {
