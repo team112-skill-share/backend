d0968d3aa0feaf4151be76488e2031ae13695d5f
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-10-04 16:57:33 +0300

    CI fix 6.0

--- src/test/java/mate/academy/skillshare/SkillShareApplicationTests.java
+++ src/test/java/mate/academy/skillshare/SkillShareApplicationTests.java
@@ -1,26 +1,10 @@
 package mate.academy.skillshare;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SkillShareApplicationTests {
-    @BeforeAll
-    static void setUp() {
-        if (System.getenv("CI") == null) {
-            // Not in CI, load from .env file
-            Dotenv dotenv = Dotenv.configure().load();
-            System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-            System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-        } else {
-            // In CI, load from system environment variables
-            System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
-            System.setProperty("JWT_EXPIRATION", System.getenv("JWT_EXPIRATION"));
-        }
-    }
-
     @Test
     void contextLoads() {
     }
