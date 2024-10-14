6280d69f29848583fa4a009b8ecbb5250126ed26
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-10-14 20:04:15 +0300

    CI fix 2.0

--- src/test/java/mate/academy/skillshare/config/GlobalSetupExtension.java
+++ src/test/java/mate/academy/skillshare/config/GlobalSetupExtension.java
@@ -11,13 +11,9 @@ public class GlobalSetupExtension implements BeforeAllCallback {
             Dotenv dotenv = Dotenv.configure().load();
             System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
             System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
-            System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
-            System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
         } else {
             System.setProperty("JWT_SECRET", System.getenv("JWT_SECRET"));
             System.setProperty("JWT_EXPIRATION", System.getenv("JWT_EXPIRATION"));
-            System.setProperty("GOOGLE_CLIENT_ID", System.getenv("GOOGLE_CLIENT_ID"));
-            System.setProperty("GOOGLE_CLIENT_SECRET", System.getenv("GOOGLE_CLIENT_SECRET"));
         }
     }
 }
